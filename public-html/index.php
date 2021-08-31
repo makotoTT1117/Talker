3142d74d04b0b338a3e300baa79de02e28ea8443
makotoTT1117 <63085274+makotoTT1117@users.noreply.github.com>
2021-08-31 16:21:03 -0400

    sign up

--- public-html/index.php
+++ public-html/index.php
@@ -10,7 +10,60 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
   <body>
-    <h1>TALKER</h1>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-auto"><h1>TALKER | SIGN UP</h1></div>
+        </div>
+
+        <hr><br>
+
+        <div class="row">
+            <div class="col-6">
+                <form>
+                    <div class="form-group">
+                        <label for="formSignUpEmail">Email address</label>
+                        <input type="email" class="form-control" id="formSignUpEmail" placeholder="Enter your email address" required pattern="^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$
+">
+                    </div>
+                    <div class="form-group">
+                        <label for="formSignUpPassword">Password</label>
+                        <input type="password" class="form-control" id="formSignUpPassword" placeholder="Enter your password" required pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$#]).{8,16}
+" onkeyup="jsSignUpValidatePassword()">
+
+                        <input type="password" class="form-control mt-4" id="formSignUpPasswordConf" placeholder="Confirm your password" required pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*$#]).{8,16}
+" onkeyup="jsSignUpValidatePassword()">
+                    </div>
+                    <p id="password_comparison"></p>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+
+            <div class="col-6">
+                <p>Hello and welcome to Talker! We are very happy that you want to join our great community!</p>
+                <p>Please, enter your email and password. Your must have access to your email because we will send
+            a confirmation code to that address. Your password must be between 8 and 16 characters long, with at
+            least one uppercase and one lowercase character, one number and one special character (@, *, $ or #).</p>
+                <p>We hope you'll enjoy Talker!</p>
+            </div>
+        </div>
+    </div>
+
+    <script>
+      var jsSignUpPassword = document.getElementById("formSignUpPassword");
+      var jsSignUpPasswordConf = document.getElementById("formSignUpPasswordConf");
+
+      function jsSignUpValidatePassword(){
+        if(jsSignUpPassword.value != jsSignUpPasswordConf.value) {
+          jsSignUpPasswordConf.setCustomValidity("Passwords don't match!");
+		 document.getElementById("password_comparison").innerHTML = "<div class='alert alert-danger' role='alert'>Passwords don't match!</div>";
+        } else {
+          jsSignUpPasswordConf.setCustomValidity('');
+		 document.getElementById("password_comparison").innerHTML = "";
+        }
+      }
+
+    </script>
+
 
     <!-- Optional Javascript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
