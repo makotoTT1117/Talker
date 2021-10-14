5e85c5f921718010743c4ff28999e412a4583515
makotoTT1117 <63085274+makotoTT1117@users.noreply.github.com>
2021-10-14 18:21:36 -0400

    136 messaging form

--- public-html/gate.js
+++ public-html/gate.js
@@ -12,6 +12,14 @@ function jsShowInputFeedback(elementId) {
       case "formSettingsBasicsNickName":
       var feedbackMessage = "Nickname must be between 3 and 15 characters long and can contain only letters."
       break;
+
+      case "formMessagingRecipient":
+      var feedbackMessage = "Choose the email address of the recipient."
+      break;
+
+      case "formMessagingContent":
+      var feedbackMessage = "Message can not be empty and can not contain '<' and '>' characters."
+      break;
     }
   
     return feedbackMessage;
---
+++ public-html/messaging.php
@@ -0,0 +1,57 @@
+<?php
+$db_data = array($_SESSION["uid"]);
+$dbUserRow = phpFetchDB('SELECT * FROM users WHERE user_id = ?', $db_data);
+$db_data = "";
+
+if ($dbUserRow["user_verified"]==0){
+
+?>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-3">Access denied!</h1>
+        <p class="lead">You need to verify your email address before you can use this feature.</p>
+      </div>
+    </div>
+  
+<?php
+}else{
+    $db_data = array();
+    $dbRecipientsList = phpFetchAllDB('SELECT * FROM users', $db_data);
+    $db_data = "";
+   
+?>
+    <h5>Messaging</h5>
+    <hr>
+    
+    <div class="row">
+        <div class="col-lg-12">
+            <form name="formMessaging" action="messaging.ctrl.php" method="post" novalidate>
+                <div class="form-group">
+                    <label for="formMessagingRecipient">Recipient</label>
+                    <select class="form-control" id="formMessagingRecipient" name="formMessagingRecipient" onchange="jsMessagingValidateSelect('formMessagingRecipient')">
+                            <option value="default" default>Select email</option>
+                            <?php foreach ($dbRecipientsList as $dbRecipientRow) { ?>
+                                <option value="<?php echo $dbRecipientRow["user_id"]; ?>"><?php echo $dbRecipientRow["user_email"]; ?></option>
+                            <? } ?>
+
+                    </select>
+                </div>
+    
+                <div class="form-group">
+                    <label for="formMessagingContent">Message</label>
+                    <textarea class="form-control" id="formMessagingContent" name="formMessagingContent" placeholder="Write the message here. Tags are not allowed."
+                    onkeyup="jsMessagingValidateTextArea('formMessagingContent')"></textarea>
+                </div>
+    
+                <button type="submit" id="formMessagingSubmit" name="formMessagingSubmit" class="btn btn-primary btn-success">Send</button>
+            </form>
+        </div>
+    </div>
+    
+    <script src="messaging.js"></script>
+
+<?php
+}
+?>
+
